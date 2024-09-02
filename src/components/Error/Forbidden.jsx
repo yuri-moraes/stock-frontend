4a0e27b4767963e2bf7a187f2d4a222022a61f51
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-09-02 16:24:24 -0300

    fet: forbidden layout

--- src/components/Error/Forbidden.jsx
+++ src/components/Error/Forbidden.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Forbidden = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>Acesso Negado</h1>
-      <p>Você não tem permissão para acessar esta seção.</p>
-      <button onClick={() => navigate("/")}>
+    <div className="not-found-container">
+      <h1 className="not-found-title">Acesso Negado</h1>
+      <p className="not-found-message">
+        Você não tem <strong>permissão</strong> para acessar esta seção.
+      </p>
+      <button className="button is-small" onClick={() => navigate("/")}>
         Voltar para a Página Inicial
       </button>
     </div>

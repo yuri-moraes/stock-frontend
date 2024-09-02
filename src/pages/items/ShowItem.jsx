575ccc002d7eb2bb4566dded1da69377add4d0a9
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-09-02 14:16:56 -0300

    fet: add and sub unity

--- src/pages/items/ShowItem.jsx
+++ src/pages/items/ShowItem.jsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import api from "../../api";
 import DeleteButton from "../../components/DeleteButton";
 import { useStock } from "../../context/useStock";
@@ -8,22 +8,76 @@ export default function ShowItem() {
   const { id } = useParams();
   const [item, setItem] = useState(null);
   const [loading, setLoading] = useState(true);
-  const { user } = useStock(); // Obtenha o usuário do contexto
+  const [quantityChange, setQuantityChange] = useState(0);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const { user } = useStock();
+  const token = localStorage.getItem("token");
+
+  const fetchItem = useCallback(async () => {
+    try {
+      const response = await api.get(`/items/${id}`);
+      setItem(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao buscar o item:", error);
+      setLoading(false);
+    }
+  }, [id]);
 
   useEffect(() => {
-    const fetchItem = async () => {
-      try {
-        const response = await api.get(`/items/${id}`);
-        setItem(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Erro ao buscar o item:", error);
-        setLoading(false);
-      }
-    };
-
     fetchItem();
-  }, [id]);
+  }, [fetchItem]);
+
+  useEffect(() => {
+    if (message || error) {
+      const timer = setTimeout(() => {
+        setMessage("");
+        setError("");
+      }, 8000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [message, error]);
+
+  const handleIncrement = () => {
+    setQuantityChange(quantityChange + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantityChange > 0) {
+      setQuantityChange(quantityChange - 1);
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      if (quantityChange !== 0) {
+        const endpoint =
+          quantityChange > 0 ? `/items/${id}/add` : `/items/${id}/subt`;
+
+        await api.patch(
+          endpoint,
+          { unity: Math.abs(quantityChange) },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setMessage("Quantidade atualizada com sucesso!");
+        setError("");
+        setQuantityChange(0);
+        await fetchItem();
+      } else {
+        setError("Nenhuma alteração de quantidade foi feita.");
+      }
+    } catch (error) {
+      setError("Erro ao atualizar a quantidade. Tente novamente.");
+      setMessage("");
+    }
+  };
 
   if (loading) {
     return <div>Carregando...</div>;
@@ -47,9 +101,27 @@ export default function ShowItem() {
 
       <div className="row">
         <span>Categoria: {item.category}</span>
-        <span>Quantidade em estoque: {item.unity}</span>
+        <div className="quantity-control">
+          <span>Quantidade em estoque: {item.unity + quantityChange}</span>
+          <button className="button is-small " onClick={handleDecrement}>
+            -
+          </button>
+          <button className="button is-small" onClick={handleSave}>
+            Salvar
+          </button>
+          {user.role === "admin" && (
+            <button className="button is-small " onClick={handleIncrement}>
+              +
+            </button>
+          )}
+        </div>
+
         <span>Preço: R$ {item.price.toFixed(2)}</span>
       </div>
+
+      {error && <p className="error-message">{error}</p>}
+      {message && <p className="success-message">{message}</p>}
+
       <p>{item.description}</p>
       <div className="row">
         <p>Cadastrado em: {new Date(item.createdAt).toDateString()}</p>
